8dc6dd21ce91f2c0723fd87df3fa621cc9875052
Slavik <slavik.grazyk@gmail.com>
2020-06-06 20:51:27 +0300

    Add SecurityContext

---
+++ DAL/CCL/Security/SecurityContext.cs
@@ -0,0 +1,21 @@
+﻿using notarius.Security.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace notarius.Security
+{
+    public static class SecurityContext
+    {
+        static User _user = null;
+
+        public static User GetUser()
+        {
+            return _user;
+        }
+        public static void SetUser(User user)
+        {
+            _user = user;
+        }
+    }
+}
