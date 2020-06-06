f15761ab95389b240a48d8f92705d8df09c58c0f
Slavik <slavik.grazyk@gmail.com>
2020-06-06 14:21:42 +0300

    Add dealContext

---
+++ DAL/DAL/EF/dealContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using notarius.DAL.Entities;
+
+namespace DAL.EF
+{
+    public class dealContext
+        : DbContext
+    {
+        public DbSet<deal> Deals { get; set; }
+        public DbSet<clients> Clients { get; set; }
+        public DbSet<services> Services { get; set; }
+        public DbSet<services_provided> Services_provided { get; set; }
+        public dealContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
