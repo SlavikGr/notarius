97343e361cf51d745dae42a5c6ae3e463ebbe3c5
Slavik <slavik.grazyk@gmail.com>
2020-06-06 16:40:54 +0300

    Create IUnitOfWork

---
+++ DAL/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IclientRepository clients { get; }
+        IdealRepository deal { get; }
+        IservicesRepository services { get; }
+        Iservices_providedRepository services_provided { get; }
+        void Save();
+
+    }
+}
