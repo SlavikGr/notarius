9ddba301faea6fdf220278d68c04d17172e2937d
Slavik <slavik.grazyk@gmail.com>
2020-06-07 15:31:17 +0300

    Final

--- BLL.Tests/DealServiceTests.cs
+++ BLL.Tests/DealServiceTests.cs
@@ -23,12 +23,67 @@ namespace BLL.Tests
         {
             // Arrange
             IUnitOfWork nullUnitOfWork = null;
-            
-    // Act
-    // Assert
-    Assert.Throws<ArgumentNullException>(
-        () => new DealService(nullUnitOfWork)
-    );
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new DealService(nullUnitOfWork));
+        }
+
+        [Fact]
+        public void GetDeals_UserIsAdmin_ThrowMethodAccessException()
+        {
+            // Arrange
+            User user = new Admin(1, "test", 1);
+            SecurityContext.SetUser(user);
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            IDealService dealService = new DealService(mockUnitOfWork.Object);
+
+            // Act
+            // Assert
+            Assert.Throws<MethodAccessException>(() => dealService.GetDeal(0));
+        }
+
+        [Fact]
+        public void GetDeals_StreetFromDAL_CorrectMappingToStreetDTO()
+        {
+            // Arrange
+            User user = new Admin(1, "test", 1);
+            SecurityContext.SetUser(user);
+            var dealService = GetDealService();
+
+            // Act
+            var actualDealDto = dealService.GetDeal(0).First();
+
+            // Assert
+            Assert.True(
+                actualDealDto.DealID == 1
+                && actualDealDto.Summa == 123
+                && actualDealDto.DealDate == "24.05.2020"
+                && actualDealDto.ClientID == 2
+                );
+        }
+
+        IDealService GetDealService()
+        {
+            var mockContext = new Mock<IUnitOfWork>();
+            var expectedDeal = new deal() { DealID = 1, Summa = 123, DealDate = "24.05.2020", ClientID = 2 };
+            var mockDbSet = new Mock<IdealRepository>();
+            mockDbSet.Setup(z =>
+                z.Find(
+                    It.IsAny<Func<deal, bool>>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>()))
+                  .Returns(
+                    new List<deal>() { expectedDeal }
+                    );
+            mockContext
+                .Setup(context =>
+                    context.deal)
+                .Returns(mockDbSet.Object);
+
+            IDealService streetService = new DealService(mockContext.Object);
+
+            return streetService;
         }
     }
 }
