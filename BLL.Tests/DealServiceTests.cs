01383fe0ee65a7f43c01bb06086fedad217ca9d6
Slavik <slavik.grazyk@gmail.com>
2020-06-07 14:20:06 +0300

    Add DealService2

---
+++ BLL.Tests/DealServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using BLL.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.EF;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using DAL.UnitOfWork;
+using BLL.DTO;
+using notarius.Security;
+using System.Security.Permissions;
+using notarius.Security.Identity;
+using AutoMapper;
+using Xunit;
+using BLL.Services.Impl;
+using Moq;
+using DAL.Repositories.Interfaces;
+using notarius.DAL.Entities;
+using deal = notarius.DAL.Entities.deal;
+
+namespace BLL.Tests
+{
+    public class DealServiceTests
+    {
+        [Fact]
+        public void Ctor_InputNull_ThrowArgumentNullException()
+        {
+            // Arrange
+            IUnitOfWork nullUnitOfWork = null;
+​
+    // Act
+    // Assert
+    Assert.Throws<ArgumentNullException>(
+        () => new DealService(nullUnitOfWork)
+    );
+        }
+    }
+}
--- DAL/BLL/Services/Impl/DealService.cs
+++ DAL/BLL/Services/Impl/DealService.cs
@@ -3,61 +3,88 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using notarius.DAL.Entities;
-using notarius.DAL.Repositories.Interfaces;
-using DAL.EF;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using DAL.UnitOfWork;
 using BLL.DTO;
-using CCL.Security;
-using System.Security.Permissions;
-using CCL.Security.Identity;
+using DAL.Repositories.Interfaces;
 using AutoMapper;
+using DAL.UnitOfWork;
+using notarius.Security;
+using notarius.Security.Identity;
 
-
-namespace BLL.Services.Impl
+namespace notarius.BLL.Services.Impl
 {
     public class DealService
         : IDealService
     {
         private readonly IUnitOfWork _database;
         private int pageSize = 10;
+
         public DealService(
             IUnitOfWork unitOfWork)
         {
             if (unitOfWork == null)
             {
-                throw new ArgumentNullException(nameof(unitOfWork));
+                throw new ArgumentNullException(
+                    nameof(unitOfWork));
             }
             _database = unitOfWork;
         }
 
         /// <exception cref="MethodAccessException"></exception>
-        public IEnumerable<dealDTO> GetSellers(int pageNumber)
+        public IEnumerable<dealDTO> GetDeals(int pageNumber)
         {
-            var deal = SecurityContext.GetDeal();
-            var login = deal.GetType();
-            if (login != typeof(Owner))
+            var user = SecurityContext.GetUser();
+            var userType = user.GetType();
+            if (userType != typeof(Admin))
             {
                 throw new MethodAccessException();
             }
-            var ServiceId = deal.ServiceID;
-            var ServiceEntities =
+            var osbbId = user.OSBBID;
+            var dealsEntities =
                 _database
-                    .services
-                    .Find(z => z.ServiceID == ServiceId, pageNumber, pageSize);
+                    .deal
+                    .Find(z => z.DealID == osbbId, pageNumber, pageSize);
             var mapper =
                 new MapperConfiguration(
-                    cfg => cfg.CreateMap<services, servicesDTO>()
+                    cfg => cfg.CreateMap<deal, dealDTO>()
                     ).CreateMapper();
-            var servicesDTO =
+            var dealsDto =
                 mapper
-                    .Map<IEnumerable<services>, List<servicesDTO>>(
-                        ServiceEntities);
-            return (IEnumerable<servicesDTO>)servicesDTO;
+                    .Map<IEnumerable<deal>, List<dealDTO>>(
+                        dealsEntities);
+            return dealsDto;
+        }
 
+        public void AddDeal(dealDTO deal)
+        {
+            var user = SecurityContext.GetUser();
+            var userType = user.GetType();
+            if (userType != typeof(Admin))
+            {
+                throw new MethodAccessException();
+            }
+            if (deal == null)
+            {
+                throw new ArgumentNullException(nameof(deal));
+            }
 
+            validate(deal);
 
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<dealDTO, deal>()).CreateMapper();
+            var dealEntity = mapper.Map<dealDTO, deal>(deal);
+            _database.deal.Create(dealEntity);
+        }
+
+        private void validate(dealDTO deal)
+        {
+            if (string.IsNullOrEmpty(deal.DealDate))
+            {
+                throw new ArgumentException("DATE повинне містити значення!");
+            }
+        }
+
+        public IEnumerable<dealDTO> GetDeal(int page)
+        {
+            throw new NotImplementedException();
         }
     }
 }
